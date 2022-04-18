7d93882c0e42f62cf2a687cb01be21162006014b
yusuke804 <14au064f@al.rikkyo.ac.jp>
2022-04-18 22:42:16 +0900

    feat: UserモデルとMeditateモデルのリレーション

--- src/app/Models/Meditate.php
+++ src/app/Models/Meditate.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Meditate extends Model
 {
     use HasFactory;
-    private $table = "meditate";
-    private $primaryKey = "meditate_id";
+    protected $table = "meditate";
+    protected $primaryKey = "meditate_id";
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
